08302b2724842d3ec6de0d9a9657228182fc8601
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-01 03:58:52 +0600

    this

--- this.js
+++ this.js
@@ -7,3 +7,13 @@ document.getElementById('btn-click2').addEventListener('click', function(){
 
 // ==> sadaron vaba kno akta object er knoo akta method er vitora this bolta oi object ka bujay...
 // ==> regular method er modda this oi buject ka bujaba kinu arrow function er modda oi taka bujba na o arak level a upora giya window ka dakaba....
+
+// const obj = { a: 1, b: 7, c: 3, length: 2 };
+// console.log(Object.keys(obj).length);
+
+// const obj1 = { module: 35, video: 2 };
+// const obj2 = { module: 35, video: 2 };
+// console.log(obj1 === obj2);
+
+// const getGirlFriend = (name = "ca") => "name";
+// console.log(getGirlFriend());
